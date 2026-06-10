f05d7a1123b96a1f678f21a8c5c04c2b67dbdbe4
Jood Jumah <jjumah@andrew.cmu.edu>
2026-06-10 18:04:13 +0400

    add function 2

--- calc.py
+++ calc.py
@@ -1,4 +1,9 @@
 """ Edit the function below to implement the String Calculator TDD Kata """
 
 def add(numbers):
-    return 0
+    if numbers == "":
+      return 0
+    total=0
+    for num in numbers.split(","):
+      total+=int(num)
+    return total
