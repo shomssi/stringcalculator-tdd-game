c8d329a4e087e1a8bc76915dddce8dce4fb30a5b
Ababiya324 <ababiyadarge@gmail.com>
2026-06-10 17:33:36 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -3,4 +3,9 @@
 def add(numbers):
     if numbers=="":
         return 0
-    return sum(int(number) for number in numbers.replace("\n",",").split(","))
+    delimiter =","
+    parts=["", numbers]
+    if numbers.startswith("//"):
+        parts=numbers.split("\n", 1)
+        delimiter=parts[0][2:]
+    return sum(int(number) for number in parts[1].replace("\n",delimiter).split(delimiter))
